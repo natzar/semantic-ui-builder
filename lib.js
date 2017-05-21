0d6458e2854e584f6b4f851182d054b95ee0855a
Beto <beto@sharplaunch.com>
2017-05-22 01:06:15 +0200

    few

--- lib.js
+++ lib.js
@@ -1,46 +1,45 @@
 
-function loadUser() {
-  var template = $('#template').html();
-  Mustache.parse(template);   // optional, speeds up future uses
-  var rendered = Mustache.render(template, {name: "Luke"});
-  $('#target').html(rendered);
-}
-
-var Template = {
-
-	render: function(template, data){
-		return Mustache.render(template,data);
-		// var output = "";
-		// for (var x in data) {
-		// 	console.log(x, data[x]);
-		// 	template = template.replace('{{'+x+'}}',data[x]);			
-		// }
-		// return template;
-	}
+function TemplateEngineRender(template, data){
+		return Mustache.render(template,data);	
 }
 
 function Ui (){
 	this.version = 1;
-	this.id = null;
+	
+	
 	this.tree = [];
 }
 
-function Group(){
-	
-}
-Group.prototype = Object.create(Ui.prototype);
 
 function Element(){
 	Ui.call(this);
 	this.view ='view';
 	this.children = [];
-	this.setId = function(){
-		//random ID
-	}
-	this.data = {};
-
+	
+	this.payload = {output: ''};
+	this.template = null;
 	this.append = function (c){
-		this.children.push(c);
+		this.children.push(c);		
+	}
+	var self = this;
+	this.render = function(){
+		var output = "";
+		if (this.children.length > 0){
+			this.children.forEach(function(item){
+				if (item.children.length>0){
+					item.children.forEach(function(sitem){
+						output += TemplateEngineRender(sitem.template, sitem.payload);
+					});
+				}
+			output += TemplateEngineRender(item.template, item.payload);
+			});
+			
+		}
+		self.payload.output = output; 
+		document.write(TemplateEngineRender(this.template,this.payload));
+	}
+	this.show = function(){
+		self.render();
 	}
 	// this.render = function(){
 	// 	document.write(Template.render(this.template, this.data));
@@ -59,7 +58,7 @@ Button.prototype = Object.create(Element.prototype);
 
 function Header (text){
 	Element.call(this);	
-	this.data = { text: text };
+	this.payload.text = text ;
 	this.template ="<h1 class='ui header'>{{text}}</h1>";
 }
 Header.prototype = Object.create(Element.prototype);
@@ -69,14 +68,7 @@ function Container(){
 	Element.call(this);
 	this.template = '<div class="ui container">{{{content}}}</div>'
 	this.data = null;
-	output = "";
-	this.flush = function(){
-		this.children.forEach(function(item){
-			output += Template.render(item.template, item.data);
-		});
-		document.write(Template.render(this.template,{content: output}));
-		
-	}
+	output = "";	
 }
 
 Container.prototype = Object.create(Element.prototype);
---
+++ main.js
@@ -0,0 +1,15 @@
+var ui = new Ui();
+
+var container = new Container();
+var header = new Header('hello!');
+var btn = new Button('hello!');
+container.append(header);
+container.append(btn);
+container.show();
+
+
+
+
+
+
+
